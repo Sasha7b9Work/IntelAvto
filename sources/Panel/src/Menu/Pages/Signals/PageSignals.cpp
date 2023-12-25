5019ef7a7fb7223e52afec615fbbc3cc1347f5bc
DESKTOP-O9E1V9C\Sasha <Sasha7b9@tut.by>
2023-12-25 17:20:06 +0300

    refact

--- sources/Panel/src/Menu/Pages/Signals/PageSignals.cpp
+++ sources/Panel/src/Menu/Pages/Signals/PageSignals.cpp
@@ -1,6 +1,55 @@
 // 2023/12/25 16:59:58 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Menu/Pages/Signals/PageSignals.h"
+#include "Menu/MenuItemsDef.h"
 
 
+TypeSignal PageSignals::typeSignal(TypeSignal::_1);
 
+
+static void OnPress_TypeSignal()
+{
+
+}
+
+// Выбор типа сигнала
+DEF_SWITCH_8(sTypeSignal,
+    "Сигнал", "Signal",
+    "Время индикации", "Display time",
+    "1", "2a", "2b", "3a", "3b", "4", "5a", "5b",
+    "1", "2a", "2b", "3a", "3b", "4", "5a", "5b",
+    PageSignals::typeSignal, OnPress_TypeSignal
+);
+
+
+Item *items[7] =
+{
+    &sTypeSignal,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+    nullptr,
+};
+
+static Page pageIndication(items, nullptr, nullptr);
+
+Page *PageSignals::self = &pageIndication;
+
+
+pchar TypeSignal::Name(E v)
+{
+    static const pchar names[Count] =
+    {
+        "1",
+        "2a",
+        "SAEJ1113-11",
+        "3a",
+        "3b",
+        "DIN40839",
+        "16750-1",
+        "16750-1"
+    };
+
+    return names[v];
+}
