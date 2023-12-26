ac0f9ed7c8c44fa163dfc7b1a6435bc7b461c75d
Sasha7b9Work <Phuntik-it@tut.by>
2023-12-26 11:51:48 +0300

    *

--- sources/common/Menu/Pages/Signals/Signal1.cpp
+++ sources/common/Menu/Pages/Signals/Signal1.cpp
@@ -14,8 +14,8 @@ namespace PageSignals
 
         void SetParameters(Item **first)
         {
-            *first++ = &param_t1;
             *first++ = &param_Us;
+            *first++ = &param_t1;
             *first++ = nullptr;
         }
     }
@@ -28,8 +28,8 @@ namespace PageSignals
 
         void SetParameters(Item **first)
         {
-            *first++ = &param_t1;
             *first++ = &param_Us;
+            *first++ = &param_t1;
             *first++ = nullptr;
         }
     }
@@ -42,48 +42,81 @@ namespace PageSignals
 
         void SetParameters(Item **first)
         {
-            *first++ = &param_td;
             *first++ = &param_Us;
+            *first++ = &param_td;
             *first++ = nullptr;
         }
     }
 
     namespace _3a
     {
+        static Parameter param_Us("Us", "Us", gset.signals[TypeSignal::_3a].values[0]);
+
         void SetParameters(Item **first)
         {
+            *first++ = &param_Us;
             *first++ = nullptr;
         }
     }
 
     namespace _3b
     {
+        static Parameter param_Us("Us", "Us", gset.signals[TypeSignal::_3b].values[0]);
+
         void SetParameters(Item **first)
         {
+            *first++ = &param_Us;
             *first++ = nullptr;
         }
     }
 
     namespace _4
     {
+        static Parameter param_Us("Us", "Us", gset.signals[TypeSignal::_4].values[0]);
+
+        static Parameter param_t7("t7", "t7", gset.signals[TypeSignal::_4].values[1]);
+
+        static Parameter param_t9("t9", "t9", gset.signals[TypeSignal::_4].values[2]);
+
+        static Parameter param_t11("t11", "t11", gset.signals[TypeSignal::_4].values[3]);
+
         void SetParameters(Item **first)
         {
+            *first++ = &param_Us;
+            *first++ = &param_t7;
+            *first++ = &param_t9;
+            *first++ = &param_t11;
             *first++ = nullptr;
         }
     }
 
     namespace _5a
     {
+        static Parameter param_Us("Us", "Us", gset.signals[TypeSignal::_5a].values[0]);
+
+        static Parameter param_td("td", "td", gset.signals[TypeSignal::_5a].values[1]);
+
         void SetParameters(Item **first)
         {
+            *first++ = &param_Us;
+            *first++ = &param_td;
             *first++ = nullptr;
         }
     }
 
     namespace _5b
     {
+        static Parameter param_Us("Us", "Us", gset.signals[TypeSignal::_5b].values[0]);
+
+        static Parameter param_Uss("Us*", "Us*", gset.signals[TypeSignal::_5b].values[1]);
+
+        static Parameter param_td("td", "td", gset.signals[TypeSignal::_5b].values[2]);
+
         void SetParameters(Item **first)
         {
+            *first++ = &param_Us;
+            *first++ = &param_Uss;
+            *first++ = &param_td;
             *first++ = nullptr;
         }
     }
