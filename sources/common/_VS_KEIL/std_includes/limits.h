caf0d63cee1282cae9e32665eb8c7c35ccaac848
Sasha7b9Work <Phuntik-it@tut.by>
2024-01-04 15:37:02 +0300

    *

--- sources/common/_VS_KEIL/std_includes/limits.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#define UINT_MAX  0xffffffffU
-#define INT_MAX   0x7fffffff
-#define SHRT_MAX  0x7fff
--- sources/common/_VS_KEIL/std_includes/stdio.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int sprintf(char *, char *, ...);
-int snprintf(char *, int, const char *, ...);
-int sscanf(void *, void *, ...);
