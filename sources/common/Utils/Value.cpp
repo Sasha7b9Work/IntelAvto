a37e1f944f456f61bf18127ecdbe220b2e6c6a7f
Sasha7b9Work <Phuntik-it@tut.by>
2024-01-09 12:22:56 +0300

    *

--- sources/common/Utils/Value.cpp
+++ sources/common/Utils/Value.cpp
@@ -1,9 +1,44 @@
 // 2023/12/22 14:55:58 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #include "defines.h"
 #include "Utils/Value.h"
+#include "Display/Text.h"
+#include <cstdlib>
+#include <cstring>
 
 
 void Value::Draw(int x, int y)
 {
+    char string[32];
 
+    char *pointer = string;
+
+    int value = munits;
+
+    if (value < 0)
+    {
+        value = -value;
+        string[0] = '-';
+        pointer++;
+    }
+
+    if (value < 1000)
+    {
+        itoa(value, pointer, 10);
+        std::strcat(string, "m");
+    }
+    else
+    {
+        int int_value = value / 1000;
+
+        itoa(int_value, pointer, 10);
+
+        std::strcat(string, ",");
+
+        value = value - int_value * 1000;
+
+        itoa(value, string + std::strlen(string), 10);
+    }
+
+    std::strcat(string, (unit == Unit::Volts) ? "V" : "s");
+    Text(string).Write(x, y);
 }
