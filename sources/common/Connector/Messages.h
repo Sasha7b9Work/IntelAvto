1125a5ea99107b6a5ff8cc50ef8954b128ff7bff
Sasha7b9Work <Phuntik-it@tut.by>
2024-02-01 13:56:35 +0300

    *

--- sources/common/Connector/Messages.h
+++ sources/common/Connector/Messages.h
@@ -1,7 +1,24 @@
 // 2024/02/01 10:54:19 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
 #pragma once
-#include "Connector/Command.h"
 #include "Connector/Value.h"
+
+
+struct Command
+{
+    enum E
+    {
+        START_1,
+        START_2A,
+        START_2B,
+        START_3A,
+        START_3B,
+        START_4,
+        START_5A,
+        START_5B,
+        STOP,
+        Count
+    };
+};
 
 
 struct BufferMessage
