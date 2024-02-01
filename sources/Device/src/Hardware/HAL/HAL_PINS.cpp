e7c72824694c40cfe273c18cbb0fddfad1bdcf13
Sasha7b9Work <Phuntik-it@tut.by>
2024-02-01 16:43:34 +0300

    *

---
+++ sources/Device/src/Hardware/HAL/HAL_PINS.cpp
@@ -0,0 +1,3 @@
+// 2024/02/01 16:42:26 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#include "defines.h"
+#include "Hardware/HAL/HAL_PINS.h"
---
+++ sources/Device/src/Hardware/HAL/HAL_PINS.h
@@ -0,0 +1,14 @@
+// 2024/02/01 16:41:42 (c) Aleksandr Shevchenko e-mail : Sasha7b9@tut.by
+#pragma once
+
+
+struct Pin
+{
+
+};
+
+
+struct PinOut : public Pin
+{
+
+};
