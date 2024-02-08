67a56868bcbff706f21372209d148feb6e8af53a
Sasha7b9Work <Phuntik-it@tut.by>
2024-02-08 10:12:43 +0300

    *

--- sources/common/Connector/Transceiver_p.cpp
+++ sources/common/Connector/Transceiver_p.cpp
@@ -11,19 +11,22 @@ void Transceiver::Transmit(BaseMessage *message)
 {
     while (true)
     {
-        HAL_SPI1::Transmit(message->Size());                            // Передаём размер сообщения (4 байта)
-
         uint crc = message->CalculateCRC();
 
-        HAL_SPI1::Transmit(message->TakeData(), message->Size());       // Передаём сообщение
-
-        HAL_SPI1::Transmit(&crc, sizeof(crc));                          // Передаём контрольную сумму сообщения
-
-        uint8 byte = 0;
-
-        if (HAL_SPI1::Receive(&byte, 1, 10))                            // И ждём подтверждения приёма в течение 10 мс
+        if (HAL_SPI1::Transmit(message->Size()))                            // Передаём размер сообщения (4 байта)
         {
-            break;
+            if (HAL_SPI1::Transmit(message->TakeData(), message->Size()))   // Передаём сообщение
+            {
+                if (HAL_SPI1::Transmit(&crc, sizeof(crc)))                  // Передаём контрольную сумму сообщения
+                {
+                    uint8 byte = 0;
+
+                    if (HAL_SPI1::Receive(&byte, 1, 10))                    // И ждём подтверждения приёма в течение 10 мс
+                    {
+                        break;
+                    }
+                }
+            }
         }
     }
 }
