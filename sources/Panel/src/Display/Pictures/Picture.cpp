04c6c3a6a7e4b5708b32418b3fa2286e6414dfd5
Sasha7b9Work <Phuntik-it@tut.by>
2025-01-29 17:28:29 +0300

    Теперь рисунок распаковывается только один раз, пока не нужно отрисовывать другой

--- sources/Panel/src/Display/Pictures/Picture.cpp
+++ sources/Panel/src/Display/Pictures/Picture.cpp
@@ -16,71 +16,96 @@ namespace Picture
 {
     // \warn сюда нельз€ распаковать картинку больше 64 кЅ
     static uint8 buffer[1024 * 64] __attribute__ ((section("CCM_DATA")));
+    static const uint8 *prev_archive = nullptr;
+
+    static bool Uncompress(const uint8 *archive);
+
+    static unsigned long CalculateSize(const uint8 *archive);
 }
 
 
-void Picture::DrawPicure(int x, int y, const uint8 *archive)
+bool Picture::Uncompress(const uint8 *archive)
 {
-    unsigned long length_archive = 0;
-
-    if (archive == bmp_zip_Signal1)
+    if (archive == prev_archive)
     {
-        length_archive = sizeof(bmp_zip_Signal1);
+        return true;
     }
+
+    prev_archive = nullptr;
 
     mz_zip_archive zip_archive;
     std::memset(&zip_archive, 0, sizeof(zip_archive));
 
-    if (mz_zip_reader_init_mem(&zip_archive, archive, length_archive, 0))
+    if (mz_zip_reader_init_mem(&zip_archive, archive, CalculateSize(archive), 0))
     {
         mz_zip_archive_file_stat file_stat;
 
         if (mz_zip_reader_file_stat(&zip_archive, 0, &file_stat))
         {
-            if(mz_zip_reader_extract_file_to_mem(&zip_archive, file_stat.m_filename, buffer, (size_t)file_stat.m_uncomp_size, 0))
+            if (mz_zip_reader_extract_file_to_mem(&zip_archive, file_stat.m_filename, buffer, (size_t)file_stat.m_uncomp_size, 0))
             {
-#pragma pack(1)
-                struct StructureBMP
-                {
-                    uint8   magic[2];
-
-                    uint    file_size;
-                    uint16  creator1;
-                    uint16  creator2;
-                    uint    bmp_offset;
-
-                    uint    header_size;
-                    int     width;
-                    int     height;
-                    uint16  num_planes;
-                    uint16  bits_per_pixel;
-                    uint    compression;
-                    uint    bmp_byte_size;
-                    int     hres;
-                    int     vres;
-                    uint    num_colors;
-                    uint    num_important_colors;
-                };
-
-                StructureBMP *head = (StructureBMP *)buffer;
-
-                uint8 *pixel = ((uint8 *)buffer) + head->bmp_offset;
-
-                for (int j = y + head->height; j > y; j--)
-                {
-                    for (int i = x; i < x + head->width; i++)
-                    {
-                        if (*pixel++ > 1)
-                        {
-                            Point().Draw(i, j, Color::WHITE);
-                        }
-                    }
-                }
+                prev_archive = archive;
             }
         }
     }
 
     mz_zip_reader_end(&zip_archive);
 
-    return;
+    return (prev_archive != nullptr);
+}
+
+
+void Picture::DrawPicure(int x, int y, const uint8 *archive)
+{
+#pragma pack(1)
+    struct StructureBMP
+    {
+        uint8   magic[2];
+
+        uint    file_size;
+        uint16  creator1;
+        uint16  creator2;
+        uint    bmp_offset;
+
+        uint    header_size;
+        int     width;
+        int     height;
+        uint16  num_planes;
+        uint16  bits_per_pixel;
+        uint    compression;
+        uint    bmp_byte_size;
+        int     hres;
+        int     vres;
+        uint    num_colors;
+        uint    num_important_colors;
+    };
+
+    if (Uncompress(archive))
+    {
+        StructureBMP *head = (StructureBMP *)buffer;
+
+        uint8 *pixel = ((uint8 *)buffer) + head->bmp_offset;
+
+        for (int j = y + head->height; j > y; j--)
+        {
+            for (int i = x; i < x + head->width; i++)
+            {
+                if (*pixel++ > 1)
+                {
+                    Point().Draw(i, j, Color::WHITE);
+                }
+            }
+        }
+    }
+}
+
+
+unsigned long Picture::CalculateSize(const uint8 *archive)
+{
+    if (archive == bmp_zip_Signal1)
+    {
+        return sizeof(bmp_zip_Signal1);
+    }
+
+    return 0UL;
 }
