4431147c23a0c950b2155c364256df12ef1c2411
Sasha7b9Work <Phuntik-it@tut.by>
2025-02-05 09:34:20 +0300

    Подключаем VCP. Не компиилируется

--- sources/Panel/src/defines.h
+++ sources/Panel/src/defines.h
@@ -2,9 +2,9 @@
 #pragma once
 #include "no-warnings.h"
 
-#define VERSION_BUILD 18
+#define VERSION_BUILD 19
 
-#define DATE_BUILD "2025-02-05 09:14:55"
+#define DATE_BUILD "2025-02-05 09:34:20"
 
 #include <stdint.h>
 
---
+++ sources/common/VCP/VCP.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <usbd_def.h>
+
+
+class VCP
+{
+public:
+
+    static void Init();
+
+    static void SendBufferSynch(const uint8 *data, int size);
+
+    // Эта строка передаётся с завершающими символами \\r\\n
+    static void SendStringAsynch(char *format, ...);
+
+    // Строка передаётся "как есть"
+    static void SendStringAsynchRAW(char *format, ...);
+
+    static void Flush();
+
+    static USBD_HandleTypeDef handleUSBD;
+
+    static PCD_HandleTypeDef handlePCD;
+
+    static bool isConnected;
+
+    static bool cableIsConnected;
+
+private:
+    static bool PrevSendingComplete();
+
+    static void SendDataAsynch(uint8 *data, int size);
+};
