3abff4a246be2dbcd91aebb131af77733ad4668c
Sasha7b9Work <Phuntik-it@tut.by>
2025-02-11 10:48:41 +0300

    *

--- sources/Panel/src/defines.h
+++ sources/Panel/src/defines.h
@@ -2,9 +2,9 @@
 #pragma once
 #include "no-warnings.h"
 
-#define VERSION_BUILD 30
+#define VERSION_BUILD 31
 
-#define DATE_BUILD "2025-02-11 10:47:00"
+#define DATE_BUILD "2025-02-11 10:48:41"
 
 #include <stdint.h>
 
