5bfb878921b781e5c37230a493705d8db0006051
Sasha7b9Work <Phuntik-it@tut.by>
2025-05-13 15:40:05 +0300

    Удалено DEF_CHOICE_1_FULL

--- sources/Panel/src/defines.h
+++ sources/Panel/src/defines.h
@@ -2,9 +2,9 @@
 #pragma once
 #include "no-warnings.h"
 
-#define VERSION_BUILD 264
+#define VERSION_BUILD 265
 
-#define DATE_BUILD "2025-05-13 15:39:19"
+#define DATE_BUILD "2025-05-13 15:40:04"
 
 #include <stdint.h>
 
